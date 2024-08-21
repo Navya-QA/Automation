4b57b5d2b5c0f25260e585a1bf8fb72998f62fec
Navyats21 <tsnavya21@gmail.com>
2024-08-22 03:21:42 +0530

    Test

--- ExpenseAutomation/FrontEnd.sh
+++ ExpenseAutomation/FrontEnd.sh
@@ -2,13 +2,14 @@ dnf install nginx -y
 echo "Installed Nginx"
 systemctl enable nginx 
 echo "Enabled Nginx"
+cp proxy.conf /etc/nginx/default.d/expense.conf
+
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp proxy.conf /etc/nginx/default.d/expense.conf
 
 echo " restart"
 systemctl enable nginx 
